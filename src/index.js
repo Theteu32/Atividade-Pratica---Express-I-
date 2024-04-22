183fef5b8ee7efb27cafb76a4c56953a3b93c7bc
Theteu <matheusdesouzaoliveira32@gmail.com>
2024-04-22 20:02:39 -0300

    .

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 let listaDeCarros = [];
-
+let idteste = 1
 app.post('/carros', (req, res)=> {
     const modelo = req.body.modelo
     const marca = req.body.marca
@@ -35,21 +35,24 @@ app.post('/carros', (req, res)=> {
         res.status(400).send('Forma do valor informado incorreto')
     }
 
-    let idteste = 1
-
     let carro = {
         id : idteste,
         modelo : modelo,
         marca : marca,
-        ano : ano, 
-        cor : cor, 
+        ano : ano,
+        cor : cor,
         preco : preco
     }
     listaDeCarros.push(carro)
-    id++
+    idteste++
 
-    res.status(201).send(`
-    carro foi adicionado \n
-    ID: ${carro.id} | Modelo: ${modelo} | Marca: ${marca} | Ano: ${ano} | Cor: ${cor} | Preço: R$${preco}`)
+    res.status(201).send(
+        JSON.stringify({
+        Mensagem: `carro foi adicionado: ID: ${carro.id} | Modelo: ${modelo} | Marca: ${marca} | Ano: ${ano} | Cor: ${cor} | Preço: R$${preco}`,
+        data: carro,
+        teste: listaDeCarros
+    })
+    )
 })
+
 app.listen(3333,()=> console.log('Servidor rodando na porta 3333'))
