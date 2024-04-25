5b68b65eae07ec5de198a79e36d5dcc8f7687d5e
Theteu <matheusdesouzaoliveira32@gmail.com>
2024-04-24 23:00:49 -0300

    mais endpoints e adicao de conteudo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import bcrypt from 'bcrypt'
 
 const app = express()
 
@@ -8,7 +9,9 @@ app.use(cors())
 app.use(express.json())
 
 let listaDeCarros = [];
-let idteste = 1
+let idteste = 1;
+let logins = [];
+let idLogin = 1
 
 app.post('/carros', (req, res)=> {
     const modelo = req.body.modelo
@@ -90,5 +93,137 @@ app.get('/buscarMarca', (req, res) => {
     res.status(200).json({success: true, data:listaDaBusca})
 })
 
+app.put('/carros/:idBuscado', (req,res)=> {
+    const idBuscado = Number(req.params.idBuscado);
 
-app.listen(3333,()=> console.log('Servidor rodando na porta 3333'))
+    if(!idBuscado){
+        res.status(400).send(JSON.stringify({Mensagem : "Informa um Id válido"}))
+    }
+
+    const verificandoId = listaDeCarros.findIndex(listaDeCarros => listaDeCarros.id === idBuscado)
+
+    if(verificandoId === -1){
+        res.status(400).send(JSON.stringify({Mensagem : "Veículo, não encontrado. O usuário deve voltar para o menu inicial depois"}))
+    }
+
+    const corNova = req.body.corNova;
+    const precoNovo = req.body.precoNovo;
+
+    if(!corNova){
+        res.status(400).send(JSON.stringify({Mensagem : "Cor inválida"}))
+    }
+
+    if(!precoNovo){
+        res.status(400).send(JSON.stringify({Mensagem : "Forma do valor informado incorreto"}))
+    }
+
+    if(verificandoId != -1){
+        const carro = listaDeCarros[verificandoId];
+        carro.cor = corNova;
+        carro.preco = precoNovo;
+
+    }
+
+    res.status(200).send(JSON.stringify({
+        Mensagem : "Atualização feita com sucesso!",
+        data : listaDeCarros 
+    }))
+})
+
+app.delete('/carros/:idBuscado', (req,res) => {
+    const deletandoId = Number(req.params.idBuscado);
+
+    if(!deletandoId){
+        res.status(400).send(JSON.stringify({Mensagem : "Informa um Id válido"}))
+    }
+
+    const idExiste = listaDeCarros.findIndex(listaDeCarros => listaDeCarros.id === deletandoId)
+
+    if(idExiste === -1){
+        res.status(400).send(JSON.stringify({Mensagem : "Veículo, não encontrado. O usuário deve voltar para o menu inicial depois"}))
+    }
+
+    if(idExiste === -1){
+        res.status(400).send(JSON.stringify({Mensagem : "Veículo, não encontrado. O usuário deve voltar para o menu inicial depois"}))
+    }
+
+    if(idExiste != -1){
+        listaDeCarros.splice(idExiste, 1)
+    }
+
+    res.status(200).send(JSON.stringify({
+        Mensagem:"Id deletado com sucesso!",
+        data:listaDeCarros
+    }))
+})
+
+app.post('/login', async (req,res) =>{
+    const nome = req.body.nome;
+    const email = req.body.email;
+    const senha = req.body.senha;
+
+    if(!nome){
+        res.status(400).send(JSON.stringify({Mensagem:'Nome inválido'}))
+    }
+
+    if(!email){
+        res.status(400).send(JSON.stringify({Mensagem:'Email inválida'}))
+    }
+
+    if(!senha){
+        res.status(400).send(JSON.stringify({Mensagem:'Senha inválida'}))
+    }
+    
+    const verificandoEmail = logins.find((olhandoEmail)=> olhandoEmail.email === email)
+
+    if(verificandoEmail){
+        res.status(400).send(JSON.stringify({Mensagem : "Email já cadastrado, informe outro"}))
+    }
+
+    const senhaCript = await bcrypt.hash(senha, 10)
+
+    let login ={
+        id : idLogin,
+        nome : nome,
+        email : email,
+        senha : senhaCript
+    }
+
+    logins.push(login)
+    idLogin++
+
+
+    res.status(200).send(JSON.stringify({Mensagem:"Login criado com sucesso"}))
+
+    console.log(logins)
+})
+
+app.get('/login', async(req,res) =>{
+    const data = req.body
+    const email = data.email
+    const senha = data.senha
+
+    if(!email){
+        res.status(400).send(JSON.stringify({Mensagem : "Email inválido"}))
+    }
+    if(!senha){
+        res.status(400).send(JSON.stringify({Mensagem : "Senha inválida"}))
+    }
+
+    const validEmail = logins.find((buscando) => buscando.email === email)
+
+    const validSenha = await bcrypt.compare(senha, validEmail.senha)
+
+    if(!validEmail){
+        res.status(404).send(JSON.stringify({Mensagem:'Email não cadastrado'}))
+    }
+
+    if(!validSenha){
+        res.status(404).send(JSON.stringify({Mensagem:'Senha incorreta'}))
+    }
+
+    res.status(200).send(JSON.stringify({Mensagem:"Login efetuado"}))
+
+})
+
+app.listen(3333,()=> console.log('Servidor rodando na porta 3333'));
